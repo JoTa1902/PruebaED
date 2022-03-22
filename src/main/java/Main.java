2a5e1364eec9a04278a74e5ffd72be01c6432541
JoTa <josemiii99xd@gmail.com>
2022-03-22 10:38:41 +0100

    prueba

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola");
+
+        String pass = "1234";
+        Scanner sc = new Scanner (System.in);
+        String texto;
+
+        do {
+            System.out.println("Proyecto listo para GitHub");
+            texto = sc.nextLine();
+            System.out.println("He recibido "+texto);
+        }while(!texto.equals(pass));
+
+        System.out.println("Contraseña correcta");
     }
 }
