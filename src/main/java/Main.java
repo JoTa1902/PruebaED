2dbf9963fda17579cb9bb5c7e7705b26e161d6bc
JoTa <josemiii99xd@gmail.com>
2022-03-23 10:32:57 +0100

    Clase23/03

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
+import java.io.InputStream;
 import java.util.Scanner;
-
+import java.io.ByteArrayInputStream;
 
 
 
@@ -7,15 +8,34 @@ public class Main {
     public static void main(String[] args) {
 
         String pass = "1234";
-        Scanner sc = new Scanner (System.in);
+        String fakeInput = "1111\n2222\n1234";
+
+        InputStream inputStream = new ByteArrayInputStream(fakeInput.getBytes());
+        //Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner (inputStream);
+        String texto;
+
+        if (compPass(sc, pass))
+            System.out.println("Has acertado la contraseña");
+        else
+            System.out.println("No has acertado la contraseña");
+
+
+
+    }
+
+
+    private static boolean compPass(Scanner sc, String pass) {
         String texto;
 
         do {
             System.out.println("Proyecto listo para GitHub");
             texto = sc.nextLine();
-            System.out.println("He recibido "+texto);
-        }while(!texto.equals(pass));
+            System.out.println("He recibido " + texto);
+        }while(!texto.equals(pass) && sc.hasNext());
 
-        System.out.println("Correcta");
+        return texto.equals(pass);
+
     }
+
 }
