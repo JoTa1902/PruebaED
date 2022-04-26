e84cca9003e8cd973ad9b166695267853fb1d3cd
JoTa <josemiii99xd@gmail.com>
2022-04-26 13:07:48 +0200

    Clase05/04

---
+++ src/main/java/ContarLetras.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ContarLetras {
+    public static void main(String[] args) {
+        final int TAM = 10;
+        Integer[] arrayNum = new Integer[TAM];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce una frase : ");
+
+        for (int i = 0; i < TAM; i++)
+            arrayNum[i] = sc.nextInt();
+
+        System.out.println(NumRepetidos(arrayNum));
+
+
+    }
+
+    public static String NumRepetidos(Integer[] arrayNum) {
+
+        if (arrayNum == null)
+            return null;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (Integer num : arrayNum) {
+            if (map.containsKey(num)) {
+                Integer value = map.get(num);
+                value++;
+                map.replace(num, value);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        return map.toString();
+    }
+}
